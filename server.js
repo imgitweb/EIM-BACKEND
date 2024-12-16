452aa9a56cd357ac65a9f534b8499a54e8f840f7
Nitin Jain <nitinjn07@gmail.com>
2024-12-16 15:33:03 +0530

    done

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 // server.js
 const express = require("express");
 const mongoose = require("mongoose");
@@ -11,14 +13,16 @@ const PORT = 5000;
 app.use(bodyParser.json());
 app.use(cors());
 
-// MongoDB Connection
+// MongoDB Atlas Connection
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER_URL}/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority&appName=EIM`;
+
 mongoose
-  .connect("mongodb://localhost:27017/eim", {
+  .connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.error(err));
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 // Routes
 const signupRoute = require("./routes/signup");
