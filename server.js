a39a63f53fab9be7720a35332c0435ed7a060c18
Nitin Jain <nitinjn07@gmail.com>
2025-01-03 12:58:12 +0530

    Done

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
     origin: ["http://localhost:3000", "http://localhost:3001"],
-    methods: ["GET", "POST", "PUT"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
