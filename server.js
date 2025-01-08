5c54a040b79030a7c7593b8c43cc04cc36630480
Nitin <nitinjn07@gmail.com>
2025-01-08 07:46:59 +0000

    Done

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+	  origin: ["http://64.227.177.30"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
