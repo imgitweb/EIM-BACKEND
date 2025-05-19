51d544a050291d7787e6504c0955457fa67f28a3
Rahul <rjshah1902@gmail.com>
2025-05-19 14:15:39 +0530

    done

--- controller/signup/otpController.js
+++ controller/signup/otpController.js
@@ -1,4 +1,5 @@
 const { validationResult } = require("express-validator");
+const StartupModel = require("../../models/signup/SubscriptionModel");
 
 exports.sendOtp = async (req, res) => {
   const errors = validationResult(req);
@@ -12,6 +13,10 @@ exports.sendOtp = async (req, res) => {
 
     if (!/\S+@\S+\.\S+/.test(email)) {
       return res.status(400).json({ error: "Invalid email address" });
+    }
+    const startup = await StartupModel.findOne({ email });
+    if (!startup) {
+      return res.status(404).json({ error: "Email already exits" });
     }
 
     const otp = Math.floor(100000 + Math.random() * 900000).toString();
