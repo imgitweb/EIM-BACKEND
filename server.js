132e94eb9ec246880bb4b4c8f6627c61b26537d0
ankitkicode <aj0353342@gmail.com>
2025-06-25 11:33:56 +0530

    updated code

--- server.js
+++ server.js
@@ -69,6 +69,8 @@ const allowedOrigins =
         "https://incubationmasters.com",
         "https://admin.incubationmasters.com",
         "https://www.incubationmasters.com",
+        "http://localhost:3000",
+        "http://localhost:3001",
       ]
     : [
         "http://localhost:3000",
