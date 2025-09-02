b837c02887daa4d5cd9febb75591ea04a641f1d3
Rahul <rjshah1902@gmail.com>
2025-09-02 12:43:11 +0530

    done

--- routes/partnerRoutes.js
+++ routes/partnerRoutes.js
@@ -1,12 +1,17 @@
-const express = require('express');
-const { createPartner, getAllPartners, getPartnerById, updatePartner, deletePartner } = require('../controller/Partner/PartnerController');
+const express = require("express");
+const {
+  createPartner,
+  getAllPartners,
+  getPartnerById,
+  updatePartner,
+  deletePartner,
+} = require("../controller/Partner/partnerController");
 const router = express.Router();
 
-
-router.post('/', createPartner);
-router.get('/', getAllPartners);
-router.get('/:id', getPartnerById);
-router.put('/:id', updatePartner);
-router.delete('/:id', deletePartner);
+router.post("/", createPartner);
+router.get("/", getAllPartners);
+router.get("/:id", getPartnerById);
+router.put("/:id", updatePartner);
+router.delete("/:id", deletePartner);
 
 module.exports = router;
