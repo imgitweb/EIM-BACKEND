acdb12ebb1ff599cf425c8681f3174e0bb2dca76
Your Name <your-email@example.com>
2025-11-03 18:27:06 +0530

    done

--- routes/matrixRoutes.js
+++ routes/matrixRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const multer = require("multer");
-const { createOrUpdateMrr, getMRR } = require("../controller/matrixController");
+const { createOrUpdateMrr, getMRR , createOrUpdateGr , getGR} = require("../controller/matrixController");
 
 const router = express.Router();
 const upload = multer();
 router.post("/add_mrr", createOrUpdateMrr);
 router.get("/get_mrr/:startup_id", getMRR);
+router.post ("/add_gr", createOrUpdateGr )
+router.get ("/get_gr/:startup_id", getGR )
 module.exports = router;
