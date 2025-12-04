4a265da7733444955154c5def0ed809c3b06dd59
Your Name <your-email@example.com>
2025-12-04 11:27:56 +0530

    done

--- models/Valuation.js
+++ models/Valuation.js
@@ -24,7 +24,7 @@ const ValuationSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
-    required: true
+    required: false 
   },
   calculatedAt: {
     type: Date,
